0b25e7157777461d6fead6fe15487cf2a972cd32
Reda ZAITI <reda.zaiti@thalesgroup.com>
2024-01-12 10:12:39 +0100

    replace Timer by Alarm for channel closing timeout

--- keymint/2.0/hal/SessionTimer.h
+++ keymint/2.0/hal/SessionTimer.h
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
-#include <signal.h>
-#include <time.h>
+#include <csignal>
 #include <unistd.h>
 #include <atomic>
 
 class Timer {
 public:
-    Timer() : timer_id(0), is_running(false) {}
+    Timer() : is_running(false) {}
 
     ~Timer() {
         stop();
@@ -18,30 +17,15 @@ public:
     void start(int timeout_ms, void* ptr) {
         if (!is_running) {
             is_running = true;
-
-            // Set up the timer
-            struct sigevent sev;
-            sev.sigev_notify = SIGEV_THREAD;
-            sev.sigev_value.sival_ptr = ptr;
-            sev.sigev_notify_function = &timerCallback;
-            sev.sigev_notify_attributes = nullptr;
-
-            struct itimerspec its;
-            its.it_value.tv_sec = timeout_ms / 1000;
-            its.it_value.tv_nsec = (timeout_ms % 1000) * 1000000;
-            its.it_interval.tv_sec = 0;
-            its.it_interval.tv_nsec = 0;
-
-            if (timer_create(CLOCK_REALTIME, &sev, &timer_id) == -1) {
-                std::cerr << "Failed to create timer" << std::endl;
-                is_running = false;
+            transport_ptr = ptr;
+            
+            if (std::signal(SIGALRM, &timerCallback) == SIG_ERR) {
+                LOG(ERROR) << "Error setting up signal handler for SIGALRM. " << std::endl;
                 return;
             }
-
-            if (timer_settime(timer_id, 0, &its, nullptr) == -1) {
-                std::cerr << "Failed to set timer" << std::endl;
-                timer_delete(timer_id);
-                is_running = false;
+            
+            if (alarm(timeout_ms / 1000) != 0) {
+                LOG(ERROR) << "Error setting the alarm. " << std::endl;
                 return;
             }
         }
@@ -51,19 +35,22 @@ public:
     void stop() {
         if (is_running) {
             is_running = false;
-            timer_delete(timer_id);
+            alarm(0);
         }
     }
 
 private:
-    timer_t timer_id;
     std::atomic<bool> is_running;
+    static void* transport_ptr;
 
     // Static callback function required by timer_create
-    static void timerCallback(sigval sv) {
-        keymint::javacard::OmapiTransport *transport = (keymint::javacard::OmapiTransport*)sv.sival_ptr;
+    static void timerCallback(int signal) {
+        LOG(DEBUG) << "signal: " << signal;
+        keymint::javacard::OmapiTransport *transport = (keymint::javacard::OmapiTransport*)transport_ptr;
         if (transport != nullptr) {
             transport->closeConnection();
         }
     }
 };
+
+void* Timer::transport_ptr = nullptr;
