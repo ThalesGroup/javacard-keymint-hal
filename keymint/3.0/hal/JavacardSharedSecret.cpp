64ea59acefb9f3ccaf1100f1386a4aaa02157185
Reda ZAITI <reda.zaiti@thalesgroup.com>
2024-09-18 10:01:58 +0200

    Unlock getSharedSecretParameters(...) during boot after a specific number of retries.

--- keymint/3.0/hal/JavacardSharedSecret.cpp
+++ keymint/3.0/hal/JavacardSharedSecret.cpp
@@ -4,27 +4,42 @@
 #include <android-base/logging.h>
 
 #include <KeyMintUtils.h>
+#define MAX_GET_SHARED_PARAM_RETRIES 60
 
 namespace aidl::android::hardware::security::sharedsecret {
 using ::keymint::javacard::Instruction;
 
+static uint8_t sharedSecretParamCounter = 0;
+
 ScopedAStatus JavacardSharedSecret::getSharedSecretParameters(SharedSecretParameters* params) {
+    std::vector<uint8_t> opt_seed;
+    std::vector<uint8_t> opt_nonce(32, 0);
+
     auto error = card_->initializeJavacard();
-    if (error != KM_ERROR_OK) {
+    if (error != KM_ERROR_OK && (sharedSecretParamCounter < MAX_GET_SHARED_PARAM_RETRIES)) {
         LOG(ERROR) << "Error in initializing javacard.";
+        sharedSecretParamCounter++;
         return keymint::km_utils::kmError2ScopedAStatus(error);
     }
     auto [item, err] = card_->sendRequest(Instruction::INS_GET_SHARED_SECRET_PARAM_CMD);
-    if (err != KM_ERROR_OK) {
+    if (err != KM_ERROR_OK && (sharedSecretParamCounter < MAX_GET_SHARED_PARAM_RETRIES)) {
         LOG(ERROR) << "Error in sending in getSharedSecretParameters.";
+        sharedSecretParamCounter++;
         return keymint::km_utils::kmError2ScopedAStatus(err);
     }
     auto optSSParams = cbor_.getSharedSecretParameters(item, 1);
-    if (!optSSParams) {
+    if (!optSSParams  && (sharedSecretParamCounter < MAX_GET_SHARED_PARAM_RETRIES)) {
         LOG(ERROR) << "Error in sending in getSharedSecretParameters.";
+        sharedSecretParamCounter++;
         return keymint::km_utils::kmError2ScopedAStatus(KM_ERROR_UNKNOWN_ERROR);
     }
-    *params = std::move(optSSParams.value());
+    if (sharedSecretParamCounter < MAX_GET_SHARED_PARAM_RETRIES)
+        *params = std::move(optSSParams.value());
+    else {
+        params->seed.clear();
+        params->nonce.assign(opt_nonce.begin(), opt_nonce.end());
+        sharedSecretParamCounter = 0;
+    }
     return ScopedAStatus::ok();
 }
 
